efa64b59b3c550446ae34fe00529b0fa17682df1
tuphan-dn <tuphan-dn@users.noreply.github.com>
2024-08-16 08:20:00 +0000

    deploy: d06a986eccc7dfe600c477534cb5dabb0dd17447

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKD/N0VtJRKkgsyVCyUkrOSSwuTi3WB4nqZZTk5ijpKGVn5qUoWRkaWdTqwHU5urgEuQYHIzSWJRZlJiblpBbrQ6VQdRsb1cYCAMt7m2B0AAAA"
