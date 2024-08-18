622998dc14d346f1f34ed0a040743d8aff272a33
tuphan-dn <tuphan@descartes.network>
2024-08-18 13:49:06 +0700

    chore: snapshot-based tests

--- test/WVIC.ts
+++ test/WVIC.ts
@@ -2,7 +2,7 @@ import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
 
-describe('WVIC', () => {
+describe('WVIC', async function () {
   async function deployFixture() {
     const WVIC = await ethers.getContractFactory('WVIC')
     const wvic = await WVIC.deploy()
@@ -12,28 +12,34 @@ describe('WVIC', () => {
     return { wvic, owner, zero, one }
   }
 
-  it('wrap', async () => {
-    const { wvic, owner, one } = await loadFixture(deployFixture)
-    await expect(
-      owner.sendTransaction({
-        to: wvic.target,
-        value: one,
-      }),
-    )
-      .to.emit(wvic, 'Transfer')
-      .withArgs(ethers.ZeroAddress, owner.address, one)
-    expect(await wvic.balanceOf(owner.address)).to.equal(one)
-    expect(await wvic.totalSupply()).to.equal(one)
-  })
+  describe('snapshot #1', async function () {
+    let snapshot: Awaited<ReturnType<typeof deployFixture>>
 
-  it('unwrap', async () => {
-    const { wvic, owner, zero, one } = await loadFixture(deployFixture)
-    await owner.sendTransaction({ to: wvic.target, value: one })
-    console.log(await wvic.burn(zero))
-    await expect(wvic.burn(one))
-      .to.emit(wvic, 'Transfer')
-      .withArgs(owner.address, ethers.ZeroAddress, one)
-    expect(await wvic.balanceOf(owner.address)).to.equal(zero)
-    expect(await wvic.totalSupply()).to.equal(zero)
+    before(async () => {
+      snapshot = await loadFixture(deployFixture)
+    })
+
+    it('wrap', async function () {
+      const { wvic, owner, one } = snapshot
+      await expect(
+        owner.sendTransaction({
+          to: wvic.target,
+          value: one,
+        }),
+      )
+        .to.emit(wvic, 'Transfer')
+        .withArgs(ethers.ZeroAddress, owner.address, one)
+      expect(await wvic.balanceOf(owner.address)).to.equal(one)
+      expect(await wvic.totalSupply()).to.equal(one)
+    })
+
+    it('unwrap', async function () {
+      const { wvic, owner, one, zero } = snapshot
+      await expect(wvic.burn(one))
+        .to.emit(wvic, 'Transfer')
+        .withArgs(owner.address, ethers.ZeroAddress, one)
+      expect(await wvic.balanceOf(owner.address)).to.equal(zero)
+      expect(await wvic.totalSupply()).to.equal(zero)
+    })
   })
 })
